1701ab02f3c42ea27f6a16e6e17744f528cdd167
Kyle Brown <brownkylej@gmail.com>
2020-02-17 11:27:02 -0500

    Finish assignment 3

--- pseudocode/delete_once.cc
+++ pseudocode/delete_once.cc
@@ -19,7 +19,29 @@ void Delete_Once(String& s, char c) {
 /// Write Delete_Once() using only set 2 operations
 void Delete_Once(String& s, char c) {
     int pos = Pos(s, c);
+    for (int i = 0; i < Length(s); i++) {
+        if (i != pos)
+            Append(s, Get_Char(s, i));
+    }
 }
 
 // Part 3
 /// Write Delete_Once() using only set 3 operations
+void Delete_Once(String& s, char c) {
+    String replacement;
+    bool deleted = false;
+    int pos = 0;
+
+    while (!Is_Empty(s)) {
+        char temp = Delete_First(s);
+        if (temp != c || (temp == c && deleted)) {
+            Insert_at(s, temp, pos);
+            pos++;
+        }
+        else if (temp == c && !deleted) {
+            deleted = true;
+        }
+    }
+
+    s = replacement;
+}
